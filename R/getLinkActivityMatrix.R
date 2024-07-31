6eabfa1123ec385eaa95be6670824484b3cc15df
IsabelleSeufert <49637511+isaLa42@users.noreply.github.com>
2024-07-31 13:03:33 +0200

    Vignettes + getLinkActivityMatrix

--- R/getLinkActivityMatrix.R
+++ R/getLinkActivityMatrix.R
@@ -4,25 +4,25 @@
 #' @param CoAccessibility The co-accessibility data produced by RWireX method getCoAccessibility.
 #' @param useMatrix Same matrix type that was used for the accessibility.
 #' @param peakSubset If specified, then the matrix is subsetted by the links with the first peak in the peakSubset.
-#' @param rangeForSubset If specified, then the matrix is subsetted by the links where the first peak is in this range.
+#' @param regionSubset If specified, then the matrix is subsetted by the links where the first peak is in this range.
 #' @param correlationCoefficient multiply links by correlation.
 #' @param binarize if matrix needs to be binarized. Have to be the same as the matrix used for CoAccessibility calculation.
 #' 
 #' @export
 
 
-getLinkActivityMatrix <- function (ArchRProj, CoAccessibility, useMatrix="PeakMatrix", peakSubset=NULL, rangeForSubset=NULL, binarize=TRUE, correlationCoefficient=FALSE, logFile = createLogFile("getLinkActivityMatrix"), verbose=TRUE){
+getLinkActivityMatrix <- function (ArchRProj, CoAccessibility, useMatrix="PeakMatrix", peakSubset=NULL, regionSubset=NULL, binarize=TRUE, correlationCoefficient=FALSE, logFile = createLogFile("getLinkActivityMatrix"), verbose=TRUE){
   ArchR:::.validInput(input = ArchRProj, name = "ArchRProj", valid = c("ArchRProj"))
   ArchR:::.validInput(input = CoAccessibility, name = "CoAccessibility", valid = c("list"))
   ArchR:::.validInput(input = useMatrix, name = "useMatrix", valid = c("character"))
   ArchR:::.validInput(input = peakSubset, name = "peakSubset", valid = c("list", "null"))
-  ArchR:::.validInput(input = rangeForSubset, name = "rangeForSubset", valid = c("GRanges", "GRangesList", " IntegerRanges", "null"))
+  ArchR:::.validInput(input = regionSubset, name = "regionSubset", valid = c("GRanges", "GRangesList", " IntegerRanges", "null"))
   ArchR:::.validInput(input = binarize, name = "binarize", valid = c("logical"))
   ArchR:::.validInput(input = correlationCoefficient, name = "correlationCoefficient", valid = c("logical"))
   ArchR:::.validInput(input = verbose, name = "verbose", valid = c("logical"))
 
   if (class(CoAccessibility) == "DFrame"){
-    CoAccessibility = .createLoopsSameChromosome(CoAccessibility)
+    CoAccessibility <- .createLoopsSameChromosome(CoAccessibility)
   }
   
   tstart <- Sys.time()
@@ -34,24 +34,26 @@ getLinkActivityMatrix <- function (ArchRProj, CoAccessibility, useMatrix="PeakMa
     stop("The order of peaks in matrix and CoAccessibility Feature Set is not the same. The lookup method is not implemented yet.")
   }
   matrix_data = matr@assays@data$PeakMatrix
-  if (!is.null(peakSubset)&&!is.null(rangeForSubset)){
+  if (!is.null(peakSubset)&&!is.null(regionSubset)){
     stop("Please choose only one subsetting option: either provide a peak list or a range.")
   }
   
-  CoAccessibility = CoAccessibility$CoAccessibility
+  CoAccessibility <- CoAccessibility$CoAccessibility
   if (!is.null(peakSubset)){
-    index_of_subset = S4Vectors::match(peakSubset, matr@rowRanges)
-    CoAccessibility = CoAccessibility[CoAccessibility$peaksetLookUpIndex1 %in% index_of_subset, ]
+    index_of_subset <- S4Vectors::match(peakSubset, matr@rowRanges)
+    CoAccessibility <- CoAccessibility[CoAccessibility$peaksetLookUpIndex1 %in% index_of_subset, ]
   }
-  if (!is.null(rangeForSubset)){
-    index_of_subset = which(!is.na(findOverlaps(matr@rowRanges, rangeForSubset, select="first")))
-    CoAccessibility = CoAccessibility[CoAccessibility$peaksetLookUpIndex1 %in% index_of_subset, ]
+  if (!is.null(regionSubset)){
+    index_of_subset <- which(!is.na(findOverlaps(matr@rowRanges, regionSubset, select="first")))
+    CoAccessibility <- CoAccessibility[CoAccessibility$peaksetLookUpIndex1 %in% index_of_subset, ]
   }
   
   ArchR:::.logDiffTime(main = "Generating Link Matrix", t1 = tstart, verbose = verbose, logFile = logFile)
-  link_matrix = matrix_data[CoAccessibility$peaksetLookUpIndex1,] * matrix_data[CoAccessibility$peaksetLookUpIndex2,]
+  link_matrix <- matrix_data[CoAccessibility$peaksetLookUpIndex1,] * matrix_data[CoAccessibility$peaksetLookUpIndex2,]
+  link_matrix@Dimnames[[1]] <- paste0(CoAccessibility$seqnames, ":", CoAccessibility$idx1, "-",
+                                      CoAccessibility$seqnames, ":", CoAccessibility$idx2)
   if (correlationCoefficient){
-    link_matrix = link_matrix * CoAccessibility$correlation
+    link_matrix <- link_matrix * CoAccessibility$correlation
   }
   
   ArchR:::.endLogging(logFile = logFile)
